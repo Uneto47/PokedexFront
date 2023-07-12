51bd0f61c3062c89e182dbda27b059776b49d1b2
Uneto47 <netovasconcelos02@gmail.com>
2023-07-11 21:25:17 -0300

    new filter in pokemnos search

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,27 +1,22 @@
 import './index.css'
-import React,{ useState } from "react";
+import React from "react";
 
 const SearchBar = (props) => {
-    const [search, setSearch] = useState("")
     const {HandleOnSearch} = props
 
     const handleOnChange = (e) => {
-        setSearch(e.target.value)
         if(e.target.value.length === 0 ) {
             HandleOnSearch(undefined)
+        } else {
+            HandleOnSearch(e.target.value.toLocaleLowerCase())
+            
         }
     }
-
-    const HandleOnClickButton = () => {
-        HandleOnSearch(search.toLocaleLowerCase())
-    }
+    
     return (
         <div className='searchbar-container'>
             <div className='searchbar'>
                 <input placeholder='Buscar pokemon...' onChange={handleOnChange} />
-            </div>
-            <div className='search-button'>
-                <button onClick={HandleOnClickButton}>Buscar</button>
             </div>
         </div>
     )
