e75606cf47f14877d4e43225564310876f3aa1c8
Uneto47 <netovasconcelos02@gmail.com>
2023-07-13 11:41:28 -0300

    Add rout function and side searchbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,8 +1,11 @@
 import './index.css'
 import SearchBar from '../SearchBar';
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = (props) => {
+    const navigate = useNavigate()
     const { HandleOnSearch } = props
+    const { hideSearch } = props
     const imgLogo =
         "https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg"
     return (
@@ -11,8 +14,10 @@ const NavBar = (props) => {
                 <img
                     alt="logo-pokemon"
                     src={imgLogo}
-                    className="navbar-img" />
-                <SearchBar HandleOnSearch={HandleOnSearch} />
+                    className="navbar-img" 
+                    onClick={()=> navigate("/")}/>
+                {!hideSearch && (
+                <SearchBar HandleOnSearch={HandleOnSearch}/>)}
             </nav>
         </div>
     )
